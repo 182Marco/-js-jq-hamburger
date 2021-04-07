16dba8af1c4a535214b37d456577c6a4512e61c9
182Marco <marcomilza@gmail.com>
2021-04-07 14:23:39 +0200

    con animazione slide in

--- main.js
+++ main.js
@@ -3,12 +3,23 @@ var hambur = $(`.fa-bars`);
 var mobileM = $(`.hamburger-menu`);
 var xSimbol = $(`.fa-times`);
 
-console.log(hambur);
+// METODO SENZA ANIMAZIONI
 
+// hambur.click(() => {
+//     mobileM.addClass('show');
+//   });
+
+//   xSimbol.click(() => {
+//     mobileM.removeClass('show');
+//   });
+
+// METODO CON SLIDE IN
 hambur.click(() => {
-  mobileM.addClass('show');
+  mobileM.addClass('show slide-left');
+  mobileM.removeClass('slide-back');
 });
 
 xSimbol.click(() => {
-  mobileM.removeClass('show');
+  mobileM.removeClass('slide-left');
+  mobileM.addClass('slide-back');
 });
