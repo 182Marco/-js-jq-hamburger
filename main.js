858579088e878bd474c9dd7f29d600ca08022a2b
182Marco <marcomilza@gmail.com>
2021-04-07 14:51:59 +0200

    fatto con 5 metodi

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ var xSimbol = $(`.fa-times`);
 // tutti i link mobile
 var linksMob = $(`.linkMob`);
 
-// METODO SENZA ANIMAZIONI
+// // METODO 1
+// // METODO CHE AGISCE DIRETTAMENTE SU STYLE IN-LINE DISPLAY SENZA PASSARE DALLE CLASSI
+
+// hambur.click(() => {
+//   mobileM.show();
+// });
+
+// xSimbol.click(() => {
+//   mobileM.hide();
+// });
+
+// // METODO 2
+// // METODO SENZA ANIMAZIONI AGGIUNGENDO E TOGLIENDO CLASSI
 
 // hambur.click(() => {
 //     mobileM.addClass('show');
@@ -15,7 +27,31 @@ var linksMob = $(`.linkMob`);
 //     mobileM.removeClass('show');
 //   });
 
-// METODO CON SLIDE IN
+// // METODO 3
+// // UTILIZZARE LE TRANSIZIONI CHE PORTA Jquery IN SLIDE (inguardabile)
+// hambur.click(() => {
+//   mobileM.slideDown(2000);
+//   mobileM.show();
+// });
+
+// xSimbol.click(() => {
+//   mobileM.slideUp(2000);
+// });
+
+// // METODO 4
+// // UTILIZZARE LE TRANSIZIONI CHE PORTA Jquery IN OPACITY
+
+// hambur.click(() => {
+//   mobileM.fadeIn();
+//   mobileM.show();
+// });
+
+// xSimbol.click(() => {
+//   mobileM.fadeOut();
+// });
+
+// METODO 5
+// METODO CON SLIDE IN ATTRAVERSO CLASSI
 hambur.click(() => {
   mobileM.addClass('show slide-left');
   mobileM.removeClass('slide-back');
